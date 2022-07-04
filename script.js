70000c1136f17af4b963d1eb7d61295938a30b04
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-04 15:52:16 -0300

    cria a função createPixels

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let pixelsConteiner = document.getElementById('pixels-container');
+const pixelBoard = document.getElementById('pixel-board');
+
+function createPixels() {
+  for (let index = 0; index < 25; index += 1) {
+    const pixels = document.createElement('div');
+    pixels.className = 'pixel';
+    pixelBoard.appendChild(pixels);
+  }
+}
+createPixels();
